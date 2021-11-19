8749db4bc16f1e38409bf8c9cc8847b16f1e054c
sai05 <dudgns35111802@gmail.com>
2021-11-19 13:49:13 +0900

    joopush

---
+++ app/src/main/java/com/jungsoo/thisisit/CategoryDetailAsiaFragment.kt
@@ -0,0 +1,39 @@
+package com.jungsoo.thisisit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class CategoryDetailAsiaFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_category_detail_asia, container, false)
+
+        view.findViewById<Button>(R.id.randomBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryDetailAsiaFragment_to_randomFragment)
+        }
+
+        view.findViewById<Button>(R.id.questionBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryDetailAsiaFragment_to_questionFragment)
+        }
+
+        view.findViewById<Button>(R.id.categoryBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryDetailAsiaFragment_to_categoryFragment)
+        }
+
+        return view
+    }
+
+
+}
--- app/src/main/java/com/jungsoo/thisisit/CategoryFragment.kt
+++ app/src/main/java/com/jungsoo/thisisit/CategoryFragment.kt
@@ -28,6 +28,38 @@ class CategoryFragment : Fragment() {
             it.findNavController().navigate(R.id.action_categoryFragment_to_randomFragment)
         }
 
+        view.findViewById<Button>(R.id.alcoholBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailAlcoholFragment)
+        }
+
+        view.findViewById<Button>(R.id.asiaBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailAsiaFragment)
+        }
+
+        view.findViewById<Button>(R.id.chinaBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailChinaFragment)
+        }
+
+        view.findViewById<Button>(R.id.dessertBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailDessertFragment)
+        }
+
+        view.findViewById<Button>(R.id.japanBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailJapanFragment)
+        }
+
+        view.findViewById<Button>(R.id.koreaBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailKoreaFragment)
+        }
+
+        view.findViewById<Button>(R.id.totalBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailTotalFragment)
+        }
+
+        view.findViewById<Button>(R.id.westBtn).setOnClickListener {
+            it.findNavController().navigate(R.id.action_categoryFragment_to_categoryDetailWestFragment)
+        }
+
         return view
     }
 
