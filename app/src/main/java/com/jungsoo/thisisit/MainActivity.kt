dd3f4c4db3b19bca25200047996642cf359f8471
sai05 <dudgns35111802@gmail.com>
2021-11-19 18:50:55 +0900

    joo2

--- app/src/main/java/com/jungsoo/thisisit/MainActivity.kt
+++ app/src/main/java/com/jungsoo/thisisit/MainActivity.kt
@@ -12,14 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*초기화면에서 회원가입 버튼을 누르면 회원가입 화면으로 넘어간다.*/
-        val joinBtn = findViewById<Button>(R.id.joinBtn)
-        joinBtn.setOnClickListener {
-            val intent =Intent(this, JoinActivity::class.java)
-            startActivity(intent)
-        }
-
-
 
 
     }
