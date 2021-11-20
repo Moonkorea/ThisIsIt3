ef5c7948cb35766b47ac1d47100486b6ffac1fec
sai05 <dudgns35111802@gmail.com>
2021-11-21 03:04:04 +0900

    login

--- app/src/main/java/com/jungsoo/thisisit/JoinActivity.kt
+++ app/src/main/java/com/jungsoo/thisisit/JoinActivity.kt
@@ -3,12 +3,50 @@ package com.jungsoo.thisisit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        val joinBtn = findViewById<Button>(R.id.btnRegister)
+        joinBtn.setOnClickListener {
+            val email = findViewById<EditText>(R.id.editId)
+            val password = findViewById<EditText>(R.id.editPw)
+            // val age = findViewById<EditText>(R.id.editAge)
+
+            Log.d("MAIN", email.text.toString())
+            Log.d("MAIN", password.text.toString())
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "회원가입 성공",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(baseContext, "회원가입 실패.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
     }
 }
--- app/src/main/java/com/jungsoo/thisisit/MainActivity.kt
+++ app/src/main/java/com/jungsoo/thisisit/MainActivity.kt
@@ -6,13 +6,59 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        auth = FirebaseAuth.getInstance()
 
+        // 비회원으로 시작
+        val btnNoLogin = findViewById<Button>(R.id.btnNoLogin)
+        btnNoLogin.setOnClickListener {
 
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 회원가입
+        val btnJoin = findViewById<Button>(R.id.btnRegister)
+        btnJoin.setOnClickListener {
+
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 로그인
+        val id = findViewById<EditText>(R.id.editId)
+        val password = findViewById<EditText>(R.id.editPw)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            if(id.text.toString().isEmpty() || password.text.toString().isEmpty()){
+                Toast.makeText(this, "아이디 또는 패스워드를 입력해주세요.", Toast.LENGTH_LONG)
+            }
+            else{
+                auth!!.signInWithEmailAndPassword(id.text.toString(), password.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful) {
+                            Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG)
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                        }
+                        else {
+                            Toast.makeText(this, "아이디 또는 패스워드를 다시 입력해주세요.", Toast.LENGTH_LONG)
+                        }
+                    }
+            }
+
+        }
     }
 }
