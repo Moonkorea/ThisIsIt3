952143f538e0378e0dd03ce1883d0b7fb5cce9d4
Moonkorea <91454751+Moonkorea@users.noreply.github.com>
2021-12-07 03:10:53 +0900

    jungsoo1207

--- app/src/main/java/com/jungsoo/thisisit/HowAboutThisActivity.kt
+++ app/src/main/java/com/jungsoo/thisisit/HowAboutThisActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_how_about_this.*
 import org.json.JSONArray
 import java.util.*
+import kotlin.random.Random.Default.nextInt
+import java.util.Random
 
 class HowAboutThisActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,12 +51,8 @@ class HowAboutThisActivity : AppCompatActivity() {
 
         if (test == 111111) {
             userAnswer = arrayOf(1, 1, 1, 1, 1, 1)
-            Log.d("useranswer", Arrays.toString(userAnswer))
 
         }
-        Log.d("useranswer", Arrays.toString(userAnswer))
-
-        Log.d("test", test.toString())
 
         for(index in 0 until jsonArray.length()){
             val jsonObject = jsonArray.getJSONObject(index)
@@ -69,7 +69,69 @@ class HowAboutThisActivity : AppCompatActivity() {
         }
         Log.d("foodquestionarray", Arrays.toString(foodQuestionArray))
 
-        val chooseBtn = findViewById<Button>(R.id.chooseBtn)
+
+        var nullCnt = 0
+        var finalFoodArrayIndex = 0
+
+
+        for(index in 0 until jsonArray.length()){
+
+            if (foodQuestionArray[index] == null) {
+                nullCnt++
+            }
+        }
+
+        var remainFoodCnt = jsonArray.length() - nullCnt
+
+
+
+        val finalFoodArray = arrayOfNulls<String>(remainFoodCnt)
+
+        for(index in 0 until jsonArray.length()){
+
+            if (foodQuestionArray[index] != null) {
+                finalFoodArray[finalFoodArrayIndex] = foodQuestionArray[index]
+                finalFoodArrayIndex++
+            }
+        }
+
+        Log.d("test2", Arrays.toString(finalFoodArray))
+
+        // 선별된 음식 화면에 출력
+
+        if (remainFoodCnt == 0) {
+            var howAboutThis = findViewById<TextView>(R.id.howAboutThis) as TextView
+            howAboutThis.setText("조건에 맞는 음식이 \n없습니다.")
+
+        } else {
+            //var randomNum = Random().nextInt(remainFoodCnt) + 1
+            val range = (0..remainFoodCnt-1)
+
+            var randomNum = range.random()
+
+            howAboutThis.setText(finalFoodArray[randomNum])
+
+            val anotherBtn = findViewById<Button>(R.id.anotherBtn)
+
+            anotherBtn.setOnClickListener {
+                var randomNum = range.random()
+                howAboutThis.setText(finalFoodArray[randomNum])
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+            val chooseBtn = findViewById<Button>(R.id.chooseBtn)
         chooseBtn.setOnClickListener {
             val intent = Intent(this, FinalResultActivity::class.java)
             //intent.putExtra("111111", 111111)
